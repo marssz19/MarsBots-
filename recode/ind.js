cd13dc6d7418e0cc12d0f640305e81e6a22e115d
marssz19 <stockrizal112@gmail.com>
2022-05-30 15:16:15 +0700

    MarsBots

---
+++ recode/ind.js
@@ -0,0 +1,12 @@
+exports.donasi = () => { 
+return`*HAI MEK BENERAN MAU DONASI?BAIK BGT SI KAMUU*
+*SEMOGA TIDAK DILIHAT DOANG*
+
+*PAYMENT BOT*
+DANA: (https://saweria.co/MarselThamVhan)
+OVO: (https://saweria.co/MarselThamVhan)
+PULSA: (https://saweria.co/MarselThamVhan)
+
+*TERIMA KASIH KAK*
+*LOVE YOU😋*`
+}
